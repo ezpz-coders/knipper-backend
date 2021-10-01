fb43bd26fbdb7cd6fbf9e183ac56b92b5de0afea
kaydee0502 <kshitijdhama513@gmail.com>
2021-10-01 21:41:52 +0530

    nodemon/dev

--- db/mongo.js
+++ db/mongo.js
@@ -8,7 +8,6 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Get the default connection
 var db = mongoose.connection;
-
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('open', () => { console.log("Mongo connected") });
