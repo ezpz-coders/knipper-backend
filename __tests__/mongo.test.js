eb5e6dbd38750375699ecb218910d9b28acf0e54
kaydee0502 <kshitijdhama513@gmail.com>
2021-10-02 23:14:07 +0530

    added connection test

--- __tests__/mongo.test.js
+++ __tests__/mongo.test.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose')
 const databaseName = 'test'
 
-beforeAll(async () => {
-    const url = `mongodb://127.0.0.1/${databaseName}`
-    await mongoose.connect(url, { useNewUrlParser: true })
-})
 
-it('connection should be defined', async () => {
-    const users = db.collection('users');
+describe("test connection", () => {
 
-    const mockUser = { _id: 'some-user-id', name: 'John' };
-    await users.insertOne(mockUser);
-
-    const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    expect(insertedUser).toEqual(mockUser);
+    let conn;
+    beforeAll(async () => {
+        const url = `mongodb://127.0.0.1/${databaseName}`
+        conn = await mongoose.connect(url, { useNewUrlParser: true })
+    })
+    afterAll(done => {
+        // Closing the DB connection allows Jest to exit successfully.
+        mongoose.connection.close()
+        done()
+    })
+    
+    it('connection should be defined', async () => {
+        expect(conn).toBeDefined();
+    })
 })
